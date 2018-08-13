52cc4a7af43bd835d8129704ab92884b3932d955
Ashkon Farhangi <ashkon.f@gmail.com>
2018-08-12 23:25:14 -0700

    curve fitting working

--- Source/main.py
+++ Source/main.py
@@ -47,21 +47,30 @@ def rank_players_by_draft_value(draft_position, remaining_player_names):
     for pair in pairs:
         print pair[0], "(draft value = %s)" % (pair[1],)
 
+def fit_curve(point_set):
+    def func(x_value, coefficient1, coefficient2, x_intercept, exponent, y_intercept):
+        return coefficient1 / numpy.power(coefficient2 * (x_value + x_intercept), exponent) + y_intercept
+        
+    x_values = point_set.x_values()
+    y_values = point_set.y_values()
+    
+    # Removing QB points that mess up function:
+    filtered_x_values = []
+    filtered_y_values = []
+    for index, x_value in enumerate(x_values):
+        y_value = y_values[index]
+        if point_set.name != "QB" or y_value > 200:
+            filtered_x_values.append(x_value)
+            filtered_y_values.append(y_value)
+
+    return (func, scipy.optimize.curve_fit(func, filtered_x_values, filtered_y_values, maxfev=1000000)[0])
+
 def main():
     players = infra.load_players()
 
     plot_data = charting.ScatterPlotData()
     for position in build_position_map(players).keys():
-        def additional_charting_behavior(point_set, x_min, x_max, x_range, x_step, y_min, y_max, y_range, color):
-            def func(x_value, power, coefficient):
-                return coefficient * (math.e ** ((x_value) * power))
-            
-            power, coefficient = scipy.optimize.curve_fit(func, point_set.x_values(), point_set.y_values(), maxfev=10000)[0]
-            lsrl_x_values = numpy.arange(x_min - x_range * 0.2, x_max + x_range * 0.2 + x_step / 2.0, x_step)
-            lsrl_y_values = [func(x_value, power, coefficient) for x_value in lsrl_x_values]
-            matplotlib.pyplot.plot(lsrl_x_values, lsrl_y_values, "--", c=color)
-    
-        point_set = charting.PointSet(position, additional_charting_behavior=additional_charting_behavior)
+        point_set = charting.PointSet(position)
         plot_data.append(point_set)
 
     for player in players:
@@ -69,6 +78,36 @@ def main():
             if source in player.projected_ppg_map:
                 point = charting.Point(player.get_rank(source), player.get_projected_ppg(source))#, player.name)
                 plot_data.add_point(point, player.position)
+
+    parameters = {}
+    functions = {}
+
+    for point_set in plot_data:
+        functions[point_set.name], parameters[point_set.name] = fit_curve(point_set)
+
+        def additional_charting_behavior(point_set, x_min, x_max, x_range, x_step, y_min, y_max, y_range, color):
+            func = functions[point_set.name]
+            params = parameters[point_set.name]
+            lsrl_x_values = numpy.arange(x_min - x_range * 0.2, x_max + x_range * 0.2 + x_step / 2.0, x_step)
+            lsrl_y_values = [func(x_value, *params) for x_value in lsrl_x_values]
+            
+            print point_set.name
+            print lsrl_x_values
+            print lsrl_y_values
+            print
+            
+            # Removing points outside first quandrant:
+            filtered_lsrl_x_values = []
+            filtered_lsrl_y_values = []
+            for index, x_value in enumerate(lsrl_x_values):
+                y_value = lsrl_y_values[index]
+                if x_value >= 0 and y_value >= 0 and y_value < 1000.0:
+                    filtered_lsrl_x_values.append(x_value)
+                    filtered_lsrl_y_values.append(y_value)
+
+            matplotlib.pyplot.plot(filtered_lsrl_x_values, filtered_lsrl_y_values, "--", c=color)
+
+        point_set.additional_charting_behavior = additional_charting_behavior
     
     charting.plot(plot_data, chart_title="Position-Specific PPG/Draft Order Trade Off Curves")
 
